112880894af948232b5b0da82194d2e7624b2765
Salma Sultana <salma@itobuz.com>
2022-12-09 14:03:02 +0530

    New commit

---
+++ pages/norating.ts
@@ -0,0 +1,15 @@
+const { I } = inject();
+
+export = {
+
+  norating() {
+
+  I.see('Feedback');
+  I.wait(2);
+  I.fillField('[data-test-id="feedback-comment"]' , 'test comment');
+  I.click('[data-testid="save-btn"]');
+  I.wait(2);
+  I.see('Feedback value is a required field');
+    
+}
+};
---
+++ pages/submit.ts
@@ -0,0 +1,24 @@
+const { I } = inject();
+
+export = {
+
+  submit() {
+
+
+    let x = Math.floor((Math.random() * 5) + 1);
+    console.log('value',x);
+    I.click(`[data-test-id="star-rating"] [data-test-id="rating-inactive"]:nth-child(${x})`);
+    I.wait(2);
+    I.fillField('[data-test-id="feedback-comment"]' , 'test comment');
+    I.click('[data-testid="save-btn"]');''
+    I.wait(2);
+    I.seeElement('[data-test-id="modal-title"]');
+    I.click('[class="text-xmd text-gray-400"]');
+    I.wait(2);
+    I.see('Events');
+    I.click('[href="/logout"]');
+    I.seeElement('[src="./pretaa-logo.png"]');
+  }
+  };
+
+
