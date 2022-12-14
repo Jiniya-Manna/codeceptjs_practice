0d0776c8c5f51a97752feb69113bfde4a0bfce51
sohail-022 <sohail@itobuz.com>
2022-12-14 18:18:57 +0530

    new

--- specs/login.ts
+++ specs/login.ts
@@ -1,8 +1,12 @@
 import loginPage = require("../pages/login");
 Feature('login');
 Scenario('PretaaLogin_test', ({ I  }) => {
+Before(async ({I}) => {
 loginPage.login()
+});
+After(async ({I}) => { 
 I.click('[href="/logout"]');
+});
 I.wait(2);
 });
 
