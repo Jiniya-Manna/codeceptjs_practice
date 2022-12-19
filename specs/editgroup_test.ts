1278cf9eda063720bea669e5ffd9cf427e221831
sohail-022 <sohail@itobuz.com>
2022-12-19 18:32:36 +0530

    text

--- specs/editgroup_test.ts
+++
@@ -1,7 +0,0 @@
-import loginPage from "../pages/login";
-import settingpagePage from "../pages/settingpage";
-Feature('creategroup');
-Scenario('go to group creation',async ({ I }) => { 
-loginPage.login()
-settingpagePage.settingpage()
-});
--- specs/login.ts
+++
@@ -1,6 +0,0 @@
-import login = require("../pages/login");
-Feature('login');
-Scenario('PretaaLogin_test', ({ I  }) => {
-login.login()
-});
-
